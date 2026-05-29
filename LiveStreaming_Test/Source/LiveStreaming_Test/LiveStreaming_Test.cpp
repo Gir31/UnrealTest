6693c0b8e89b06667e3f2608b5442d16cffc10ea
Gir31_PC <rpg2657@gmail.com>
2026-05-30 00:57:17 +0900

    Live Streaming Test

---
+++ LiveStreaming_Test/Source/LiveStreaming_Test/LiveStreaming_Test.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LiveStreaming_Test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LiveStreaming_Test, "LiveStreaming_Test" );
+
+DEFINE_LOG_CATEGORY(LogLiveStreaming_Test)
---
+++ LiveStreaming_Test/Source/LiveStreaming_Test/LiveStreaming_TestGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LiveStreaming_TestGameMode.h"
+
+ALiveStreaming_TestGameMode::ALiveStreaming_TestGameMode()
+{
+	// stub
+}
